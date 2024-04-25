cd73fd961da68c9e36441209bf3e801d49536522
Fahim Faysal <164730091+fahim-5@users.noreply.github.com>
2024-04-25 18:05:09 +0600

    Add files via upload

---
+++ Hanoi_tower_recursive.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+    if (n == 1) {
+        std::cout << "Move disk 1 from " << source << " to " << destination << std::endl;
+        return;
+    }
+    
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    std::cout << "Move disk " << n << " from " << source << " to " << destination << std::endl;
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main() {
+    int n = 3; // Number of disks
+    towerOfHanoi(n, 'A', 'B', 'C');
+    return 0;
+}
